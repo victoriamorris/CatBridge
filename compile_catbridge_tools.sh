6b80a74887390ac6b5b90dd88ecb4f87685f3fdc
victoriamorris <victoria.morris@bl.uk>
2023-12-15 16:06:15 +0000

    Add files via upload
    Updated compilation script to avoid using "python install setup.py"

--- compile_catbridge_tools.sh
+++ compile_catbridge_tools.sh
@@ -1,14 +1,14 @@
-python setup.py install
-pyinstaller bin/cn_find.py -F
-mv dist/cn_find.exe cn_find.exe
-pyinstaller bin/marc_count.py -F
-mv dist/marc_count.exe marc_count.exe
-pyinstaller bin/keep_fld.py -F
-mv dist/keep_fld.exe keep_fld.exe
-pyinstaller bin/fix_fmt.py -F
-mv dist/fix_fmt.exe fix_fmt.exe
-rm bin/__pycache__/*.cpython-37.pyc
-rmdir bin/__pycache__
+python -m PyInstaller bin/cn_find.py -F
+mv dist/cn_find.exe exe/cn_find.exe
+python -m PyInstaller bin/marc_count.py -F
+mv dist/marc_count.exe exe/marc_count.exe
+python -m PyInstaller bin/keep_fld.py -F
+mv dist/keep_fld.exe exe/keep_fld.exe
+python -m PyInstaller bin/fix_fmt.py -F
+mv dist/fix_fmt.exe exe/fix_fmt.exe
+python -m PyInstaller bin/marc_check.py -F
+mv dist/marc_check.exe exe/marc_check.exe
+rmdir -rf catbridge_tools/__pycache__
 rm -rf build
 rm *.spec
 read -p "Press [Enter]"
